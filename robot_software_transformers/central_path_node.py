b2ce95501b36c8a4919bd26eb6c669380f9ca4ef
AlienX <64488123+AlienX2001@users.noreply.github.com>
2021-09-25 13:24:43 +0530

    Update central_path_node.py

--- robot_software_transformers/central_path_node.py
+++ robot_software_transformers/central_path_node.py
@@ -1,18 +1,18 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Pose2D
-from std_msgs.msg import Bool as bool
-
+from std_msgs.msg import Bool
+import functools
 
 class CentralPathNode(Node):
     def __init__(self,robots):
         super().__init__('central_path_node')
         self.time_period = 1/30
         #self.time = self.create_timer(time_period, self.timer_callback(num))
-        self.r_1_bool_sub = self.create_subscription(bool, 'r_1/bool', self.r_1_callback, 1)
-        self.r_2_bool_sub = self.create_subscription(bool, 'r_2/bool', self.r_2_callback, 1)
-        self.r_3_bool_sub = self.create_subscription(bool, 'r_3/bool', self.r_3_callback, 1)
-        self.r_4_bool_sub = self.create_subscription(bool, 'r_4/bool', self.r_4_callback, 1)
+        self.r_1_bool_sub = self.create_subscription(Bool, 'r_1/bool', self.r_1_callback, 1)
+        self.r_2_bool_sub = self.create_subscription(Bool, 'r_2/bool', self.r_2_callback, 1)
+        self.r_3_bool_sub = self.create_subscription(Bool, 'r_3/bool', self.r_3_callback, 1)
+        self.r_4_bool_sub = self.create_subscription(Bool, 'r_4/bool', self.r_4_callback, 1)
         self.r_1_pose_sub = self.create_subscription(Pose2D, 'r_1/target', self.r_1_pose_callback, 1)
         self.r_2_pose_sub = self.create_subscription(Pose2D, 'r_2/target', self.r_2_pose_callback, 1)
         self.r_3_pose_sub = self.create_subscription(Pose2D, 'r_3/target', self.r_3_pose_callback, 1)
@@ -23,24 +23,24 @@ class CentralPathNode(Node):
         self.r_4_pose_final = self.create_publisher(Pose2D, 'r_4/final_target', 1)
         
     def r_1_callback(self,msg):
-        if(msg==True):
-            self.time = self.create_timer(self.time_period, self.timer_callback('r_1',self.r_1_pose_sub))
+        if(msg.data==True):
+            self.time = self.create_timer(self.time_period, functools.partial(self.timer_callback,content=self.r_1_pose_sub))
     
     def r_2_callback(self,msg):
-        if(msg==True):
-            self.time = self.create_timer(self.time_period, self.timer_callback('r_2',self.r_2_pose_sub))
+        if(msg.data==True):
+            self.time = self.create_timer(self.time_period, functools.partial(self.timer_callback,content=self.r_2_pose_sub))
     
     def r_3_callback(self,msg):
-        if(msg==True):
+        if(msg.data==True):
             
-            self.time = self.create_timer(self.time_period, self.timer_callback('r_3',self.r_3_pose_sub))
+            self.time = self.create_timer(self.time_period, functools.partial(self.timer_callback,content=self.r_3_pose_sub))
     
     def r_4_callback(self,msg):
-        if(msg==True):
+        if(msg.data==True):
             
-            self.time = self.create_timer(self.time_period, self.timer_callback('r_4',self.r_4_pose_sub))
+            self.time = self.create_timer(self.time_period, functools.partial(self.timer_callback,content=self.r_4_pose_sub))
             
-    def timer_callback(self, rID, content):
+    def timer_callback(self, content):
         self.r_1_pose_final.publish(content)
         
 def main():
