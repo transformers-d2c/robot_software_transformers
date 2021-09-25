79a8e4db3f1d6dc6fe9d4c09bcd1bfa35e1c08fc
AlienX <64488123+AlienX2001@users.noreply.github.com>
2021-09-25 23:49:43 +0530

    Update central_path_node.py

--- robot_software_transformers/central_path_node.py
+++ robot_software_transformers/central_path_node.py
@@ -6,9 +6,9 @@ import functools
 
 class CentralPathNode(Node):
     def __init__(self,robots):
-        super().__init__('central_path_node')
+        super().__init__('central_path_node') # making node called cpn
         self.time_period = 1/30
-        #self.time = self.create_timer(time_period, self.timer_callback(num))
+        """subscribing to all bool topics and target topics"""
         self.r_1_bool_sub = self.create_subscription(Bool, 'r_1/bool', self.r_1_callback, 1)
         self.r_2_bool_sub = self.create_subscription(Bool, 'r_2/bool', self.r_2_callback, 1)
         self.r_3_bool_sub = self.create_subscription(Bool, 'r_3/bool', self.r_3_callback, 1)
@@ -17,6 +17,7 @@ class CentralPathNode(Node):
         self.r_2_pose_sub = self.create_subscription(Pose2D, 'r_2/target', self.r_2_pose_callback, 1)
         self.r_3_pose_sub = self.create_subscription(Pose2D, 'r_3/target', self.r_3_pose_callback, 1)
         self.r_4_pose_sub = self.create_subscription(Pose2D, 'r_4/target', self.r_4_pose_callback, 1)
+        """making a publisher to publish in final_target topics"""
         self.r_1_pose_final = self.create_publisher(Pose2D, 'r_1/final_target', 1)
         self.r_2_pose_final = self.create_publisher(Pose2D, 'r_2/final_target', 1)
         self.r_3_pose_final = self.create_publisher(Pose2D, 'r_3/final_target', 1)
@@ -24,30 +25,33 @@ class CentralPathNode(Node):
         
     def r_1_callback(self,msg):
         if(msg.data==True):
+            """if the bool data is true meaning bot1 is active hence will work or else wont"""
             self.time = self.create_timer(self.time_period, functools.partial(self.timer_callback,content=self.r_1_pose_sub))
     
     def r_2_callback(self,msg):
         if(msg.data==True):
+            """if the bool data is true meaning bot2 is active hence will work or else wont"""
             self.time = self.create_timer(self.time_period, functools.partial(self.timer_callback,content=self.r_2_pose_sub))
     
     def r_3_callback(self,msg):
         if(msg.data==True):
-            
+            """if the bool data is true meaning bot3 is active hence will work or else wont"""
             self.time = self.create_timer(self.time_period, functools.partial(self.timer_callback,content=self.r_3_pose_sub))
     
     def r_4_callback(self,msg):
         if(msg.data==True):
-            
+            """if the bool data is true meaning bot4 is active hence will work or else wont"""
             self.time = self.create_timer(self.time_period, functools.partial(self.timer_callback,content=self.r_4_pose_sub))
             
     def timer_callback(self, content):
+        """This function will simply recieve the Pose2D object as content and then publish it in new topic final_target"""
         self.r_1_pose_final.publish(content)
         
 def main():
-    rclpy.init()
-    central_path_node = CentralPathNode()
-    rclpy.spin(central_path_node)
-    rclpy.shutdown()
+    rclpy.init() # init rclpy
+    central_path_node = CentralPathNode() # init object of CentralPathNode
+    rclpy.spin(central_path_node) # spin the object
+    rclpy.shutdown() # shutdown
 
 if __name__ == '__main__':
     main()
