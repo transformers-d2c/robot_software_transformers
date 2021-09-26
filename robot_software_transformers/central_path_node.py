673d9caf369c424b210814c31569719afcac53f5
AlienX <64488123+AlienX2001@users.noreply.github.com>
2021-09-26 12:34:45 +0530

    Update central_path_node.py

--- robot_software_transformers/central_path_node.py
+++ robot_software_transformers/central_path_node.py
@@ -9,10 +9,10 @@ class CentralPathNode(Node):
         super().__init__('central_path_node') # making node called cpn
         self.time_period = 1/30
         """subscribing to all bool topics and target topics"""
-        self.r_1_bool_sub = self.create_subscription(Bool, 'r_1/bool', self.r_1_callback, 1)
-        self.r_2_bool_sub = self.create_subscription(Bool, 'r_2/bool', self.r_2_callback, 1)
-        self.r_3_bool_sub = self.create_subscription(Bool, 'r_3/bool', self.r_3_callback, 1)
-        self.r_4_bool_sub = self.create_subscription(Bool, 'r_4/bool', self.r_4_callback, 1)
+        self.r_1_bool_sub = self.create_subscription(Bool, 'r_1/Bool', self.r_1_callback, 1)
+        self.r_2_bool_sub = self.create_subscription(Bool, 'r_2/Bool', self.r_2_callback, 1)
+        self.r_3_bool_sub = self.create_subscription(Bool, 'r_3/Bool', self.r_3_callback, 1)
+        self.r_4_bool_sub = self.create_subscription(Bool, 'r_4/Bool', self.r_4_callback, 1)
         self.r_1_pose_sub = self.create_subscription(Pose2D, 'r_1/target', self.r_1_pose_callback, 1)
         self.r_2_pose_sub = self.create_subscription(Pose2D, 'r_2/target', self.r_2_pose_callback, 1)
         self.r_3_pose_sub = self.create_subscription(Pose2D, 'r_3/target', self.r_3_pose_callback, 1)
