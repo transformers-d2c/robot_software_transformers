bf99e73a6a21ea7f0d0fe7539bc96718d623d2d1
aryan20182 <88556682+aryan20182@users.noreply.github.com>
2022-10-07 02:26:39 +0530

    Update test_pep257.py

--- test/test_pep257.py
+++ test/test_pep257.py
@@ -13,11 +13,11 @@
 # limitations under the License.
 
 from ament_pep257.main import main
-import pytest
+import pytest as pt
 
 
-@pytest.mark.linter
-@pytest.mark.pep257
+@pt.mark.linter
+@pt.mark.pep257
 def test_pep257():
     rc = main(argv=['.', 'test'])
     assert rc == 0, 'Found code style errors / warnings'
